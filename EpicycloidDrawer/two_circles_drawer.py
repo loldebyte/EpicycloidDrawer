b8c746d1a20f04e1b3f2314e45e932c495c56697
loldebyte <qdimarellis@laposte.net>
2020-11-01 13:31:38 +0100

    PEP8

--- EpicycloidDrawer/two_circles_drawer.py
+++ EpicycloidDrawer/two_circles_drawer.py
@@ -1,4 +1,4 @@
-ch#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct 10 16:15:03 2020
@@ -9,6 +9,36 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 plt.style.use('seaborn-pastel')
+
+
+def points_on_circumference(x, y, r, precision=100):
+    return [((np.cos(2*np.pi / precision*i))*r + x,
+             (np.sin(2*np.pi / precision*i))*r + y)
+            for i in np.arange(0, precision+1)]
+
+
+def current_point_on_circle(xy: tuple, t, r, speed=1,
+                            precision=100):
+    center_x, center_y = xy[0], xy[1]
+    return (np.cos(2*np.pi/precision*(t*speed + center_x))*r,
+            np.sin(2*np.pi/precision*(t*speed + center_y))*r)
+
+
+def init():
+    c = plt.Circle((0, 0), RAYON, color='blue', fill=False)
+    c2 = plt.Circle(points[0], RAYON/2, color='blue', fill=False)
+    ax.add_artist(c)
+    ax.add_artist(c2)
+    scatter.set_offsets(np.hstack(points[0]))
+    return scatter, c, c2
+
+
+def animate(i):
+    x = [x[0] for x in points]
+    y = [y[1] for y in points]
+    scatter.set_offsets(np.hstack((x[i % N_POINTS], y[i % N_POINTS])))
+    return scatter,
+
 
 N_POINTS = 100
 RAYON = .5
@@ -21,32 +51,7 @@ ax.set_xlim([-2, 2])
 ax.set_ylim([-2, 2])
 scatter = ax.scatter([], [], c='red', marker='+')
 
-def points_on_circumference(x, y, r, precision=100):
-    return [((np.cos(2*np.pi / precision*i ))*r + x,
-             (np.sin(2*np.pi / precision*i ))*r + y)
-            for i in np.arange(0, precision+1)]
-
-def current_point_on_circle(xy: tuple, t, r, speed=1,
-                            precision=100):
-    center_x, center_y = xy[0], xy[1]
-    return (np.cos(2*np.pi/precision*(t*speed + center_x) )*r,
-            np.sin(2*np.pi/precision*(t*speed + center_y) )*r)
-
 points = points_on_circumference(0, 0, RAYON, N_POINTS)
-
-def init():
-    c = plt.Circle((0, 0), RAYON, color='blue', fill=False)
-    c2 = plt.Circle(points[0], RAYON/2, color='blue', fill=False)
-    ax.add_artist(c)
-    ax.add_artist(c2)
-    scatter.set_offsets(np.hstack(points[0]))
-    return scatter, c, c2
-
-def animate(i):
-    x = [x[0] for x in points]
-    y = [y[1] for y in points]
-    scatter.set_offsets(np.hstack((x[i%N_POINTS], y[i%N_POINTS])))
-    return scatter,
 
 anim = FuncAnimation(fig, animate, init_func=init,
                      frames=200, interval=20, blit=True)
