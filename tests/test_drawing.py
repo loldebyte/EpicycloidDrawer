accd09937a2cc639407bae39d184de9b906b9f07
loldebyte <qdimarellis@laposte.net>
2021-03-11 17:12:57 +0100

    rewrite tests using pytest-mpl doesnt work : incomplete tests are passing

--- tests/test_drawing.py
+++ tests/test_drawing.py
@@ -1,55 +1,62 @@
-from matplotlib.testing.decorators import image_comparison
-import epicycloid_drawer.core
+import pytest
+import epicycloid_drawer
 
 
 def test_one_circle_basic():
-    @image_comparison(baseline_images=['test_1circle'],
-                      extensions=['png'])
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='1circle.png')
     def test_basic_circle():
-        epicycloid_drawer.core.draw_image(r=[.5])
+        epicycloid_drawer.draw_circle()
 
-    @image_comparison(baseline_images=['test_1circle_t1'],
-                      extensions=['png'])
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='1circle_t1.png')
     def test_basic_circle_t1():
-        epicycloid_drawer.core.draw_image(r=[.5], t=1)
+        epicycloid_drawer.draw_circle(t=1)
 
 
 def test_one_circle_radius():
-    @image_comparison(baseline_images=['test_radius_1circle'],
-                      extensions=['png'])
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='test_radius_1circle_t0.png')
     def test_diameter():
         epicycloid_drawer.core.draw_image(r=[1])
 
-    @image_comparison(baseline_images=['test_radius_1circle_t1'],
-                      extensions=['png'])
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='test_radius_1circle_t1.png')
     def test_diameter_t1():
         epicycloid_drawer.core.draw_image(r=[1], t=1)
 
 
 def test_one_circle_speed():
-    @image_comparison(baseline_images=['test_speed_1circle'],
-                      extensions=['png'])
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='test_speed_1circle_t0.png')
     def test_speed():
         epicycloid_drawer.core.draw_image(r=[.5], s=10)
 
-    @image_comparison(baseline_images=['test_speed_1circle_t1'],
-                      extensions=['png'])
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='test_speed_1circle_t1.png')
     def test_speed_t1():
         epicycloid_drawer.core.draw_image(r=[.5], s=10, t=1)
 
 
 def test_one_circle_phase():
-    # TODO: add @image_comparisons to all test functions
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='test_phase_1circle_t0.png')
     def test_phase():
         epicycloid_drawer.core.draw_image(r=[.5], theta=[30])
 
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='test_phase_1circle_t1.png')
     def test_phase_t1():
         epicycloid_drawer.core.draw_image(r=[.5], theta=[30], t=1)
 
 
 def test_one_circle_all():
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='test_all_1circle_t0.png')
     def test_all():
         epicycloid_drawer.core.draw_image(r=[1], s=10, theta=[30])
 
+    @pytest.mark.mpl_image_compare(baseline_dir='baseline_images',
+                                   filename='test_all_1circle_t1.png')
     def test_all_t1():
         epicycloid_drawer.core.draw_image(r=[1], s=10, theta=[30], t=1)
